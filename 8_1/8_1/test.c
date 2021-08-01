796c497d2e7b3c8c4ce05d4d14c1656e7eee14ed
WSWAlready <1719713584@qq.com>
2021-08-01 19:30:59 +0800

    8—1

---
+++ 8_1/8_1/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	short s = 0;
+//	int a = 10;
+//	printf("%d\n",sizeof(s = a + 5));//sizeof括号中的表达式是不参与计算的
+//	printf("%d\n",s);
+//	return 0;
+//}
+
+int main()
+{
+	//int a = 0;
+	//printf("%d\n",~a);//按位取反
+	int a = 1
+
+
+	return 0 ;
+}
