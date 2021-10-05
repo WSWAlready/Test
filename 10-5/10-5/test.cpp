e557e7eed88ab130fc889371a15e8f646d1617f2
WSWAlready <1719713584@qq.com>
2021-10-05 18:57:26 +0800

    10-5 补充

--- 10-5/10-5/test.cpp
+++ 10-5/10-5/test.cpp
@@ -65,45 +65,116 @@
 //	return 0;
 //}
 
-int FindNum(int arr[3][3],int num, int* px,int* py)
+//int FindNum(int arr[3][3],int num, int* px,int* py)
+//{
+//	int x = 0;
+//	int y = *py - 1;
+//	while(x<= *px -1 && y>=*py - 3)
+//	{
+//		if(arr[x][y] < num)
+//		{
+//			x++;
+//		}
+//		else if(arr[x][y] > num)
+//		{
+//				y--;
+//		}
+//		else
+//		{
+//			*px = x;
+//			*py = y;
+//			return 1;
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+//	int k = 6;
+//	int x = 3;
+//	int y = 3;
+//	int ret = FindNum(arr,k,&x,&y);
+//	if(ret == 1)
+//	{
+//		printf("找到了\n");
+//		printf("下标是 %d %d\n",x,y);
+//
+//	}
+//	else
+//	{
+//		printf("找不到\n");
+//	}
+//	return 0;
+//}
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while(*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	
+//	int len = my_strlen("abcdef");
+//	printf("%d\n",len);
+//}
+//strlen 的类型是size_t就是unsigned int，无符号整型
+
+//char* my_strcpy(char* dest,char* src)
+//{
+//	char* ret = dest;
+//	while(*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//
+//
+//
+//int main()
+//{
+//	char arr1[] = "abcdefghi";
+//	char arr2[] = "bit";
+//	my_strcpy(arr1,arr2);
+//	printf("%s\n",arr1);
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	char arr1[] = "hello";
+//	char arr2[] = "world";
+//	strcat(arr1,arr2);
+//	printf("%s\n",arr1);//这样写会造成错误，原因就是arr1里面的内存不够，导致越界访问
+//
+//}
+
+char * my_strcat(char* dest,char* src)
 {
-	int x = 0;
-	int y = *py - 1;
-	while(x<= *px -1 && y>=*py - 3)
+	char* set = dest;
+	//找到目的字符串的'\0'
+	while(*dest != '\0')
 	{
-		if(arr[x][y] < num)
-		{
-			x++;
-		}
-		else if(arr[x][y] > num)
-		{
-				y--;
-		}
-		else
-		{
-			*px = x;
-			*py = y;
-			return 1;
-		}
+		dest++;
 	}
-	return 0;
+	while(*dest++ = *src++)
+	{
+		;
+	}
+	return set;
 }
+
 int main()
 {
-	int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-	int k = 6;
-	int x = 3;
-	int y = 3;
-	int ret = FindNum(arr,k,&x,&y);
-	if(ret == 1)
-	{
-		printf("找到了\n");
-		printf("下标是 %d %d\n",x,y);
-
-	}
-	else
-	{
-		printf("找不到\n");
-	}
-	return 0;
+	char arr1[30] = "hello";
+	char arr2[] = "world";
+	my_strcat(arr1,arr2);
+	printf("%s\n",arr1);
 }
