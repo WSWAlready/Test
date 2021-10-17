2590a9ec6a978322d1775575e00785876c9122d8
wangershuai <466478796@qq.com>
2021-10-17 19:26:36 +0800

    10-17

--- 10-12/10-12/test.c
+++ 10-12/10-12/test.c
@@ -33,5 +33,124 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	int*p = realloc(NULL,40);//malloc(40);
+//	return 0;
+//
+//}
 
+//int main()
+//{
+//	int i = 0;
+//	//1.对空指针的解引用操作
+//	int *p = malloc(40);
+//	*p = 10;//malloc开辟空间失败，对NULL指针解引用
+//
+//	//2.对动态开辟内存的越界访问
+//	int *p2 = (int*)malloc(40);
+//	if(p = = NULL)
+//	{
+//		return 0;
+//	}
+//
+//	//for (int  = 0;i <= 10;i++)	//会造成越界访问
+//	for (int  = 0;i < 10;i++)
+//	{
+//		*(p+i) = i;
+//	}
+//	free(p);
+//	p = NNULL;
+//	return 0;
+//	//3.对非动态开辟的内存使用free释放
+//  //4.使用free释放动态开辟内存的一部分
+//  //5.对同一块空间多次释放
+//  //6.动态开辟内存忘记释放（内存泄漏）
+//}	
+
+
+
+//void GetMemory(char *p)
+//{
+//	p = (char*)malloc(100);//p现在开辟的一个空间，空间的首地址就是p的内容。形参只有在函数内部才能生效，而且p并没有被释放，会导致内存泄漏
+//}
+//
+//
+//void Test(void)
+//{
+//	char *str = NULL;
+//	GetMemory(str);//将str的值传给p，但是实际上str的值并没被改变，还是NULL
+//	strcpy(str,"hello world");//目前str还是空指针，没有有效的空间
+//	printf(str);
+//}
+////
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
+//改正
+//void GetMemory(char **p)
+//{
+//	*p = (char*)malloc(100);
+//}
+//
+//
+//void Test(void)
+//{
+//	char *str = NULL;
+//	GetMemory(&str);
+//	strcpy(str,"hello world");
+//	printf(str);
+//}
+////
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
+//char* GetMemory(char *p)
+//{
+//	p = (char*)malloc(100);
+//	return p;
+//}
+//
+//
+//void Test(void)
+//{
+//	char *str = NULL;
+//	str = GetMemory(str);
+//	strcpy(str,"hello world");
+//	printf(str);
+//}
+////
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
+
+char *GetMemory(void)
+{
+	static char p[] = "hello world";//局部变量，出了这个函数，空间就被销毁了。
+	return p;
+}
+
+
+void Test(void)
+{
+	int i = 0;
+	char *str = NULL;
+	str = GetMemory();
+printf(str);	
+}
+//
+int main()
+{
+	Test();
+	return 0;
+}
 
